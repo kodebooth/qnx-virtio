kodebooth/qnx-virtio

commit 098387bec830441907331a1e9cc2820c0a333e42

fix: don't use physical memory for virtq struct
The virtq struct itself does not need to be available in physical
memory. Only the descriptor, available, and used rings needs to be.

Fixes: 1c6cc53 ("Initial commit")
Signed-off-by: Abe Kohandel <abe@kodebooth.com>

src/virtio/virtq.c
@@ -248,9 +248,8 @@ int virtq_create(size_t size, bool legacy, struct virtq **vq) {
 
   log_debug("creating virtqueue: size=%zu, legacy=%d", size, legacy);
 
-  pvq = mmap64(0, sizeof(struct virtq), PROT_READ | PROT_WRITE | PROT_NOCACHE,
-               MAP_SHARED | MAP_PHYS | MAP_ANON, NOFD, 0);
-  if (pvq == MAP_FAILED) {
+  pvq = calloc(1, sizeof(struct virtq));
+  if (pvq == NULL) {
     rc = errno;
     log_err("failed to allocate virtqueue structure: %s", strerror(rc));
     return rc;
@@ -258,7 +257,7 @@ int virtq_create(size_t size, bool legacy, struct virtq **vq) {
 
   rc = legacy ? virtq_create_legacy(pvq, size) : virtq_create_modern(pvq, size);
   if (rc != EOK) {
-    goto unmap_vq;
+    goto free_vq;
   }
 
   rc = mem_offset64(pvq->desc, NOFD, virtq_desc_mem_size(pvq->num),
@@ -327,8 +326,8 @@ extras:
 destroy:
   legacy ? virtq_destroy_legacy(pvq) : virtq_destroy_modern(pvq);
 
-unmap_vq:
-  munmap(pvq, sizeof(struct virtq));
+free_vq:
+  free(pvq);
 
   return rc;
 }

commit 74038732219e660dc78e3d3c6227157d2cc39c5f

fix: free allocated virtq struct
Free the allocated virtq structure instead of unmapping it as it is now
allocated using calloc and no longer uses mmap64.

Fixes: 098387b ("fix: don't use physical memory for virtq struct")
Signed-off-by: Abe Kohandel <abe@kodebooth.com>

src/virtio/virtq.c
@@ -356,10 +356,7 @@ int virtq_destroy(struct virtq *const vq) {
     return rc;
   }
 
-  rc = munmap(vq, sizeof(struct virtq));
-  if (rc != EOK) {
-    return errno;
-  }
+  free(vq);
 
   return EOK;
 }